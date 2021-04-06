e1a9490e9735ebf7b634330417b5aa09cc34e68b
shrivatsaBhatP <shrivatsa@robosoftin.com>
2021-04-06 23:41:11 +0530

    router location updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
+import Routes from './router/routes';
 import './styles/index.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Searchpage from './pages/Searchpage';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/search">
-          <Searchpage />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <Routes />;
 };
 
 export default App;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Searchpage from '../pages/Searchpage';
 
 const Routes: React.FC = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/search">
+          <Searchpage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default Routes;
