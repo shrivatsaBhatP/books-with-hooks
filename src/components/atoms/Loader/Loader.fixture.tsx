c4aab59d5017fa372dd244a61150ca272f68fc54
shrivatsaBhatP <shrivatsa@robosoftin.com>
2021-04-07 16:37:02 +0530

    component loader added

---
+++ src/components/atoms/Loader/Loader.fixture.tsx
@@ -0,0 +1,3 @@
+import Loader from './';
+
+export default <Loader />;
---
+++ src/components/atoms/Loader/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Loader.module.css';
+
+const Loader = () => {
+  return <div className={classes['loader']}>Loading</div>;
+};
+
+export default Loader;
